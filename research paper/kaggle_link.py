367adf059092f6e03bed6e661ae754e86486c8ab
Abhijeet Anand <34609032+abbi163@users.noreply.github.com>
2021-07-02 14:17:01 +0900

    Kaggle links of contract

---
+++ research paper/kaggle_link.py
@@ -0,0 +1,6 @@
+# It was great to talk with you and thanks again for helping us. Below are some links for sample cases, but as far as I understood their contract (network) structure is a bit different from our problem. I will let you know if I can get a more closer dataset.
+# (1-n contracts)
+# https://www.kaggle.com/chicago/chicago-contracts
+# https://www.kaggle.com/theworldbank/world-banks-major-contracts
+# (n-m contracts)
+# https://www.kaggle.com/jarosawjaworski/current-nba-players-contracts-history
